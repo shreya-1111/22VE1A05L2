5268720c2e9ffab3a7274d9a1f1d6fee9868bbb5
shreya-1111 <desamsettyshreya@gmail.com>
2025-07-18 12:45:31 +0530

    Create ShortLink.jsx

---
+++ Frontend Test Submission/src/components/ShortLink.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+export default function ShortLink({url}){
+  const handleCopy=()=>{
+    navigator.clipboard.writeText(url);
+    alert('copied');
+  };
+  return(
+    <div style={styles.container}>
+      <p>Short URL:</p>
+      <a href={url} target="_blank" rel="noopener noreferrer">{url}</a>
+      <br />
+      <button onClick={handleCopy} style={styles.button}>Copy</button>
+    </div>
+  );
+}
+const styles={
+  container:{
+    marginTop:'20px',
+  },
+  button:{
+    marginTop:'10px',
+    padding:'5px 15px',
+  },
+};
